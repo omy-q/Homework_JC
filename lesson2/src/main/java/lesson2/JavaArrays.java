a86bc268ab3d384f9bf88d206d04dddd18a53f17
omy-q <olya_aparina@mail.ru>
2021-03-02 20:47:55 +0300

    first task completed

---
+++ lesson2/src/main/java/lesson2/JavaArrays.java
@@ -0,0 +1,40 @@
+package lesson2;
+
+import java.util.Arrays;
+
+public class JavaArrays {
+    public static void main(String[] args){
+
+        invertArrayFirstVar();
+        System.out.println();
+        invertArraySecondVar();
+    }
+
+    private static void invertArrayFirstVar(){
+        System.out.println("Задача1. Первый вариант решения");
+        int[] arr = {0, 1, 1, 0, 1, 0, 0};
+        System.out.println(Arrays.toString(arr) + " - исходный массив");
+        for (int i = 0; i < arr.length; i++){
+            switch (arr[i]){
+                case 0:
+                    arr[i] = 1;
+                    break;
+                case 1:
+                    arr[i] = 0;
+                    break;
+            }
+        }
+        System.out.println(Arrays.toString(arr) + " - инвертированный массив");
+    }
+
+    private static void invertArraySecondVar(){
+        System.out.println("Задача1. Второй вариант решения");
+        int[] arr = {0, 1, 1, 0, 1, 0, 0};
+        System.out.println(Arrays.toString(arr) + " - исходный массив");
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+        System.out.println(Arrays.toString(arr) + " - инвертированный массив");
+    }
+}
