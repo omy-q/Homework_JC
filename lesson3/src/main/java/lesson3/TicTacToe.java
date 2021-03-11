82a922b96ad85120564751ba8b84bc95a5d81020
omy-q <olya_aparina@mail.ru>
2021-03-11 10:33:36 +0300

    add human and ai turn and checkwin

--- lesson3/src/main/java/lesson3/TicTacToe.java
+++ lesson3/src/main/java/lesson3/TicTacToe.java
@@ -18,6 +18,21 @@ public class TicTacToe {
     public static void main(String[] args){
         initField();
         printField();
+        do{
+            humanTurn();
+            if (checkWin(dot_X)){
+                printField();
+                System.out.println("Победа игрока");
+                break;
+            }
+            aiTurn();
+            if (checkWin(dot_O)){
+                printField();
+                System.out.println("Победа искусственного интелекта");
+                break;
+            }
+            printField();
+        } while(true);
     }
 
     private static void initField(){
@@ -50,6 +65,88 @@ public class TicTacToe {
             System.out.println();
         }
         System.out.println();
+    }
+
+    private static void humanTurn(){
+        int x, y;
+        System.out.println("Введите координаты поля, куда будете ставить знак:\r");
+        x = sc.nextInt() - 1;
+        y = sc.nextInt() - 1;
+        while(!isCellValid(x,y) || !isCellEmpty(x,y)){
+            System.out.println("Вы ввели неверные координаты, повторите ввод:\r");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        }
+        field[x][y] = dot_X;
+    }
+    private static void aiTurn(){
+        int x, y;
+        do {
+            x = rand.nextInt(size);
+            y = rand.nextInt(size);
+        } while (!isCellValid(x, y) || !isCellEmpty(x, y));
+        field[x][y] = dot_O;
+    }
+    private static boolean isCellValid(int x, int y){
+        return(0<=x && x<size && 0<=y && y<size);
+    }
+    private static boolean isCellEmpty(int x, int y) {
+        return (field[x][y] == dot_EMPTY);
+    }
+    private static boolean checkWin(char dot){
+        int count = 0;
+        for (int i = 0; i < size; i++){
+            for (int j = 1; j < size; j++){
+                if (field[i][j] == dot &&  field[i][j-1] == dot) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            }
+            if (isSomebodyWin(count)){
+                return true;
+            }
+            count = 0;
+        }
+        for (int j = 0; j < size; j++){
+            for (int i = 1; i < size; i++) {
+                if (field[i][j] == dot &&  field[i-1][j] == dot) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            }
+            if (isSomebodyWin(count)){
+                return true;
+            }
+            count = 0;
+        }
+        for (int i = 1, j = 1; i < size && j < size; i++, j++){
+            if (field[i][j] == dot &&  field[i-1][j-1] == dot) {
+                count++;
+            } else {
+                count = 0;
+            }
+        }
+        if (isSomebodyWin(count)) {
+            return true;
+        }
+        count = 0;
+        for (int i = 1, j = size-2; i < size && j >= 0; i++, j--){
+            if (field[i][j] == dot &&  field[i-1][j+1] == dot) {
+                count++;
+            } else {
+                count = 0;
+            }
+        }
+        if (isSomebodyWin(count)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isSomebodyWin(int count){
+        return (count == size - 1);
     }
 
 }
