9f344030f99e78d2b8bc5b8a68923ec94b20bc5e
omy-q <olya_aparina@mail.ru>
2021-03-14 11:35:36 +0300

    create class Worker

--- lesson4/src/main/java/lesson4/Worker.java
+++ lesson4/src/main/java/lesson4/Worker.java
@@ -1,4 +1,32 @@
 package lesson4;
 
 public class Worker {
+
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int age;
+
+    private String position;
+    private String email;
+    private String telephone;
+
+    private int salary;
+
+    public Worker(String surname, String name, String patronymic, int age, String position, String email, String telephone, int salary) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.age = age;
+
+        this.position = position;
+        this.email = email;
+        this.telephone = telephone;
+        this.salary = salary;
+    }
+
+    public void getInfo(){
+        System.out.println("Surname %s name %s patronymic %s \nage %", surname, name, patronymic, age);
+        System.out.println("Position: %s \nEmail: %s \nTelephone: %ss \nSalary: %\n", position, email, telephone, salary);
+    }
 }
