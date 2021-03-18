4eb1104ecc2ab8f978891435f596062434e11ce0
omy-q <olya_aparina@mail.ru>
2021-03-18 17:35:09 +0300

    create object Dog

---
+++ lesson5/src/main/java/lesson5/Dog.java
@@ -0,0 +1,13 @@
+package lesson5;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    public Dog(String name, String color, int age) {
+        super(name, color, age);
+    }
+
+}
