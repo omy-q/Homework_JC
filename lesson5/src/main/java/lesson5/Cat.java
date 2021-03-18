aa725bf25ae58a2d04cdb32100122ecec4a4730b
omy-q <olya_aparina@mail.ru>
2021-03-18 17:47:30 +0300

    add restrictions

--- lesson5/src/main/java/lesson5/Cat.java
+++ lesson5/src/main/java/lesson5/Cat.java
@@ -10,4 +10,15 @@ public class Cat extends Animal {
         super(name, color, age);
     }
 
+    @Override
+    public void run(int distance) {
+        if (distance > 200) System.out.println("Кот не может пробежать более 200 метров");
+        else super.run(distance);
+    }
+
+    @Override
+    public void swim(int distance) {
+        System.out.println("Кот не умеет плавать");
+    }
+
 }
--- lesson5/src/main/java/lesson5/Dog.java
+++ lesson5/src/main/java/lesson5/Dog.java
@@ -10,4 +10,16 @@ public class Dog extends Animal {
         super(name, color, age);
     }
 
+    @Override
+    public void run(int distance) {
+        if (distance > 500) System.out.println("Собака не может пробежать более 500 метров");
+        else super.run(distance);
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance > 10) System.out.println("Собака не может проплыть более 10 метров");
+        else super.swim(distance);
+    }
+
 }
