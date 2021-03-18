81b9dbf3ae4abff282f024d32ad8a779b225b96e
omy-q <olya_aparina@mail.ru>
2021-03-18 17:38:08 +0300

    modified MainClass, create objects

--- lesson5/src/main/java/lesson5/MainClass.java
+++ lesson5/src/main/java/lesson5/MainClass.java
@@ -3,6 +3,16 @@ package lesson5;
 public class MainClass {
 
     public static void main(String[] args){
-        
+
+        Dog dog1 = new Dog("Хоган", "черный",7);
+        Dog dog2 = new Dog("Сэм");
+
+        Cat cat1 = new Cat("Муся", "серый", 3);
+        Cat cat2 = new Cat("Мурзик");
+
+        dog1.run(20);
+        dog2.run(5);
+        cat1.swim(25);
+        dog1.swim(16);
     }
 }
