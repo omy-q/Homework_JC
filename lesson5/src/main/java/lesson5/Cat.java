6267261fb9dfd4ada49f9ebedb5d75dc2aa7dbcc
omy-q <olya_aparina@mail.ru>
2021-03-18 17:34:41 +0300

    create object Cat

---
+++ lesson5/src/main/java/lesson5/Cat.java
@@ -0,0 +1,13 @@
+package lesson5;
+
+public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    public Cat(String name, String color, int age) {
+        super(name, color, age);
+    }
+
+}
