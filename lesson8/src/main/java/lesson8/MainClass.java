7de04aed4827da88a1d6726271ff4e946480526c
omy-q <olya_aparina@mail.ru>
2021-03-30 14:32:33 +0300

    create correct array

--- lesson8/src/main/java/lesson8/MainClass.java
+++ lesson8/src/main/java/lesson8/MainClass.java
@@ -4,22 +4,42 @@ public class MainClass {
 
     public static void main(String[] args){
 
+        String[][] correctArr = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        String[][] incorrectSizeArr1 = {};
+        String[][] incorrectSizeArr2 = {};
+        String[][] incorrectDataArr = {};
+
+        try {
+            getArraySum(correctArr);
+        } catch (MyArraySizeException e){
+            e.printStackTrace();
+        } catch (MyArrayDataException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
     private static void getArraySum(String[][] arr){
         if (arr.length != 4) throw new MyArraySizeException();
+        int sum = 0;
         for (int i = 0; i < arr.length; i++){
             if (arr[i].length != 4) throw new MyArraySizeException();
-            int sum = 0;
             for (int j = 0; j < 4; j++){
                 try {
                     sum += Integer.parseInt(arr[i][j]);
                 }
                 catch (RuntimeException e){
-                    throw new MyArrayDataException();
+                    throw new MyArrayDataException("in the cell[" + i + "][" + j + "] contains incorrect data!");
                 }
             }
         }
+        System.out.println("Сумма всех элементов двумерного массива 4х4 равна: " + sum);
     }
 
 }
