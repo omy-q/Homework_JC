daa765d0674b1d5562c2db47aa4a72a47f07ffaa
omy-q <olya_aparina@mail.ru>
2021-03-30 14:13:07 +0300

    change method addArraySum

--- lesson8/src/main/java/lesson8/MainClass.java
+++ lesson8/src/main/java/lesson8/MainClass.java
@@ -6,5 +6,20 @@ public class MainClass {
 
     }
 
-    private static void getSum(String[][] arr){}
+    private static void getArraySum(String[][] arr){
+        if (arr.length != 4) throw new MyArraySizeException();
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i].length != 4) throw new MyArraySizeException();
+            int sum = 0;
+            for (int j = 0; j < 4; j++){
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                }
+                catch (RuntimeException e){
+                    throw new MyArrayDataException();
+                }
+            }
+        }
+    }
+
 }
