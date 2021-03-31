aa75d1701c38fcf10b00c81ab2624df2620afb21
omy-q <olya_aparina@mail.ru>
2021-03-31 20:40:05 +0300

    create class Dog

--- lesson9/src/main/java/lesson9/Dog.java
+++ lesson9/src/main/java/lesson9/Dog.java
@@ -1,4 +1,51 @@
 package lesson9;
 
 public class Dog {
+
+    private String name;
+    private String color;
+    private int age;
+    private String breed;
+
+
+
+    public Dog(String name, int age, String color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public Dog(String name, int age, String color, String breed){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.breed = breed;
+    }
+
+    public void getInfo(){
+        String str_breed = breed;
+        if (str_breed == null) str_breed = "дворняжка";
+        System.out.printf("Информация по данной собаке:\n имя: %s;\n " +
+                                                         "возраст: %d;\n" +
+                                                         "цвет: %s;\n" +
+                                                         "порода: %s\n",
+                         name, age, color, str_breed);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getBreed(){
+        return breed;
+    }
+
 }
