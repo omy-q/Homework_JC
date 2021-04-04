8fa9daee8fa6e8d855207b6c7e22f4453f824626
omy-q <olya_aparina@mail.ru>
2021-04-04 12:59:37 +0300

    add notes and get numbers of phonebook

--- lesson10/src/main/java/lesson10/MainClass.java
+++ lesson10/src/main/java/lesson10/MainClass.java
@@ -13,12 +13,27 @@ public class MainClass {
             list.add("word" + (i + 1));
         }
         System.out.println(list);
+        System.out.println();
 
         Set<String> set = new LinkedHashSet<>();
         set.addAll(list);
         System.out.println(set);
+        System.out.println();
 
         System.out.println(getQuantity(list));
+        System.out.println();
+
+        PhoneBook phonebook = new PhoneBook();
+        phonebook.add("Ivanov", "+789465231");
+        phonebook.add("Kopylov", "61-26-03");
+        phonebook.add("Kopylov", "+79456123502");
+        phonebook.add("Suslova", "231645");
+        phonebook.add("Ivanov", "12-56-78");
+
+        phonebook.get("Ivanov");
+        phonebook.get("kopylov");
+        phonebook.get("Suslova");
+        phonebook.get("Bobrova");
     }
 
     public static LinkedHashMap getQuantity(List<String> words){
