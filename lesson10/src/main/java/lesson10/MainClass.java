995b65dcfdfc382964c4c7d6348d41275cb3c868
omy-q <olya_aparina@mail.ru>
2021-04-04 12:18:02 +0300

    create array of 20 words

--- lesson10/src/main/java/lesson10/MainClass.java
+++ lesson10/src/main/java/lesson10/MainClass.java
@@ -1,8 +1,19 @@
 package lesson10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainClass {
 
     public static void main(String[] args){
-        
+        List<String> list = new ArrayList<>(20);
+        for(int i = 0; i < 5; i++){
+            list.add("word" + (i + 1));
+            list.add("word" + (i + 1));
+            list.add("word" + (i + 1));
+            list.add("word" + (i + 1));
+        }
+        System.out.println(list);
+
     }
 }
