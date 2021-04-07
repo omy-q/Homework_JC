a0de042aa24c15bc1cbe5120ce62568822994ab9
omy-q <olya_aparina@mail.ru>
2021-04-07 18:10:30 +0300

    add method for swap element in array

--- lesson11/src/main/java/lesson11/MainClass.java
+++ lesson11/src/main/java/lesson11/MainClass.java
@@ -1,4 +1,32 @@
 package lesson11;
 
+import java.util.List;
+
 public class MainClass {
+
+    public static void main(String[] args){
+        Integer[] arr1 = {5, 9, 0};
+        String[] arr2 = {"Hello", "!", "world"};
+        printArr(arr1);
+        printArr(swapEl(arr1, 1, 0));
+        printArr(arr2);
+        printArr(swapEl(arr2, 1, 2));
+
+    }
+
+    private static <E> E[] swapEl(E[] array, int index1, int index2){
+        E buffer = array[index1];
+        array[index1] = array[index2];
+        array[index2] = buffer;
+        return array;
+    }
+
+    private static <E> void printArr(E[] arr){
+        System.out.print("[ ");
+        for (E el:arr) {
+            System.out.print(el + " ");
+        }
+        System.out.print("]\n");
+    }
+
 }
