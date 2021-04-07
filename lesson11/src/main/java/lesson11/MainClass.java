44f3445cbd09b5be52dc05593926ad3a1685b337
omy-q <olya_aparina@mail.ru>
2021-04-07 19:55:30 +0300

    create class box and work with him

--- lesson11/src/main/java/lesson11/MainClass.java
+++ lesson11/src/main/java/lesson11/MainClass.java
@@ -21,6 +21,48 @@ public class MainClass {
         System.out.println("apple weight: " + apple.getWeight());
         System.out.println("orange weight: " + orange.getWeight());
 
+        Apple[] apples = {new Apple(0.56f),
+                          new Apple(1.23f),
+                          new Apple(0.35f),
+                          new Apple(0.64f)};
+
+        Orange[] oranges = {new Orange(0.85f),
+                            new Orange(1.15f),
+                            new Orange(0.96f),
+                            new Orange(1.29f),
+                            new Orange(0.76f)};
+
+
+        Box<Apple> boxWithApple1 = new Box<>("Apples", apples);
+        Box.printBox(boxWithApple1);
+        Box<Orange> boxWithOrange1 = new Box<>("Oranges", oranges);
+        Box.printBox(boxWithOrange1);
+
+        Apple appleAdd = new Apple(0.46f);
+        boxWithApple1.addFruit(appleAdd);
+        Box.printBox(boxWithApple1);
+
+        Orange orangeAdd = new Orange(0.63f);
+        boxWithOrange1.addFruit(orangeAdd);
+        Box.printBox(boxWithOrange1);
+
+        System.out.println("Weight box with apples: " + boxWithApple1.getWeight());
+        System.out.println("Weight box with oranges: " + boxWithOrange1.getWeight());
+
+        System.out.println("Weight of these boxes is the same? Answer: " + boxWithApple1.compare(boxWithOrange1));
+
+        Apple[] apples2 = {new Apple(0.12f),
+                           new Apple(0.25f),
+                           new Apple(0.94f),
+                           new Apple(0.38f)};
+
+        Box<Apple> boxWithApple2 = new Box<>("Apples", apples2);
+        Box.printBox(boxWithApple2);
+
+        boxWithApple1.changeBox(boxWithApple2);
+        Box.printBox(boxWithApple1);
+        Box.printBox(boxWithApple2);
+
     }
 
     private static <E> E[] swapEl(E[] array, int index1, int index2){
