6327d3e94d5d5b0d44ce92d9ad2a4ac321fe19d9
omy-q <olya_aparina@mail.ru>
2021-04-14 19:53:21 +0300

    add some updates

--- lesson12/src/main/java/lesson12/MainClass.java
+++ lesson12/src/main/java/lesson12/MainClass.java
@@ -7,21 +7,28 @@ public class MainClass {
     public static final int SIZE = 100_000_000;
     public static final int HALF = SIZE / 2;
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws InterruptedException {
         createArr();
 
     }
 
-    public static float[] createArr(){
-
+    public static void createArr(){
         float[] arr = new float[SIZE];
         Arrays.fill(arr, 1);
         long timeStart = System.currentTimeMillis();
+
         for (int i = 0; i < SIZE; i++){
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arr[i] = changeArrayElement(arr[i], i);
         }
+
         long timeFinish = System.currentTimeMillis();
         System.out.println("Lead time: " + (timeFinish - timeStart) + "ms");
-        return arr;
     }
+
+    public static float changeArrayElement(float elem, int index){
+            elem = (float)(elem * Math.sin(0.2f + index / 5) * Math.cos(0.2f + index / 5) * Math.cos(0.4f + index / 2));
+        return elem;
+    }
+
 }
