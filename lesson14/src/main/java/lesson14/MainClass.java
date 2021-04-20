1549adc4daa74465eafa17efa9fe18cbda4b6066
omy-q <olya_aparina@mail.ru>
2021-04-20 23:15:38 +0300

    add method to create new arr with values after 4

--- lesson14/src/main/java/lesson14/MainClass.java
+++ lesson14/src/main/java/lesson14/MainClass.java
@@ -1,4 +1,22 @@
 package lesson14;
 
+import java.util.Arrays;
+
 public class MainClass {
+
+    public int[] arrayAfterFour(int[] arr) throws RuntimeException{
+        int[] newArr;
+        int startIndex = - 1;
+        for (int i = arr.length; i >= 0; i--){
+            if (arr[i] == 4){
+                startIndex = i + 1;
+            }
+        }
+        if (startIndex == - 1){
+            throw new RuntimeException();
+        } else{
+            newArr = Arrays.copyOfRange(arr, startIndex, arr.length - 1);
+        }
+        return newArr;
+    }
 }
